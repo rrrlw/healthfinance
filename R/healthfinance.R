105a4fb54f268cbec5677b9836c710d4b60ccc11
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-07-14 16:37:15 -0400

    package documentation added

---
+++ R/healthfinance.R
@@ -0,0 +1,13 @@
+#' Financial Projections and Planning for Healthcare Practices
+#' 
+#' Provides a shiny interface for a free, open-source managerial
+#' accounting-like system for healthcare practices. This package allows
+#' healthcare administrators to project revenue with monthly adjustments
+#' and procedure-specific boosts up to a 3-year period. Granular data
+#' (patient-level) to aggregated data (department- or hospital-level) can
+#' all be used as valid inputs provided historical volume and revenue
+#' data is available.
+#' 
+#' @name healthfinance
+#' @docType package
+NULL
